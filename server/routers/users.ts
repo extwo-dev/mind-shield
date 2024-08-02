e018b2be4f3e2dd64bea05f6e53830b30fc02980
Nikita Simankov <nikita.simankov.id@gmail.com>
2024-08-02 08:28:37 +0300

    Added the rest of shadcn-ui components

--- server/routers/users.ts
+++ server/routers/users.ts
@@ -1,10 +1,10 @@
-import prisma from "@/utils/prisma/client"
-import { procedure, router } from ".."
+import { prisma } from "@/utils/prisma/client";
+import { procedure, router } from "..";
 
 export const usersRouter = router({
   list: procedure.query(async () => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany();
 
     return users;
-  })
-})
+  }),
+});
